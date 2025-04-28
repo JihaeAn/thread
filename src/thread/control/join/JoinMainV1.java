89d0f5690ef9aa1576a63e41c12877a1238d6177
jihae <dkswlgo6615@naver.com>
2025-04-28 23:38:27 +0900

    쓰레드에 대하여

--- src/thread/control/join/JoinMainV1.java
+++ src/thread/control/join/JoinMainV1.java
@@ -4,6 +4,9 @@ import static thread.util.MyLogger.log;
 import static thread.util.ThreadUtils.sleep;
 
 public class JoinMainV1 {
+    // 참고!
+    // main 쓰레드가 실행한 start()는 쓰레드의 실행이 끝날 때까지 기다리지 않는다.
+    // 다른 쓰레드를 실행만 해두고, 자신의 다음 코드를 실행한다.
     public static void main(String[] args) {
         log("Start");
         SumTask task1 = new SumTask(1, 50);
@@ -20,6 +23,14 @@ public class JoinMainV1 {
         int sumAll = task1.result + task2.result;
         log("task1 + task2 = " + sumAll);
         log("End");
+
+        // 다들 0을 출력함 이유는 왤까?
+        /*
+        main 쓰레드는 thread1과 thread2에 각각 작업을 지시한다.
+        thread1과 thread2가 작업을 끝마치기도 전에 먼저 계산 결과를 조회한다.
+        참고로 sleep(2000);을 걸어놨기에 계산을 만료하는데는 2초 정도가 걸린다.
+         */
+
     }
 
     static class SumTask implements Runnable {
